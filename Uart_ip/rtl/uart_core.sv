4de213087f1dd969315e51e34763e1d02f2771cd
uzair141 <qureshi.uzair14121@gmail.com>
2022-03-25 19:12:15 +0500

    Uart Updated

--- Uart_ip/rtl/uart_core.sv
+++ Uart_ip/rtl/uart_core.sv
@@ -19,14 +19,14 @@
 //                                                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 module uart_core (
-	input logic pclk_i,
-	input logic prst_ni,
-	input logic [31:0] pwdata_i,
-	input logic [11:0] paddr_i,
-	input logic pwrite_i,
-	input logic pread_i,
+	input logic clk_i,
+	input logic rst_ni,
+	input logic [31:0] reg_wdata,
+	input logic [11:0] reg_addr,
+	input logic reg_we,
+	input logic reg_re,
 	input logic rx_i,
-	output logic [31:0] prdata_o,
+	output logic [31:0] reg_rdata,
 	output logic tx_o,
 	output logic intr_tx,
 	output logic intr_rx,
@@ -76,8 +76,8 @@ logic [2:0] r_tx_byte_done = 0;
 
 
 uart_rx urx(
-	.clk_i(pclk_i),
-	.rst_ni(prst_ni),           
+	.clk_i(clk_i),
+	.rst_ni(rst_ni),           
 	.rx_i(tx_o),               
 	.rx_en_i(rx_en_t),
 	.clks_per_bit(baud),
@@ -87,8 +87,8 @@ uart_rx urx(
 );
 
 uart_tx utx(
-	.clk_i(pclk_i),
-	.rst_ni(prst_ni),     
+	.clk_i(clk_i),
+	.rst_ni(rst_ni),     
 	.tx_en_i(tx_en),          
 	.tx_data_i(tx_fifo_o), 
 	.clks_per_bit(baud),              
@@ -97,9 +97,9 @@ uart_tx utx(
 );
 
 uart_fifo_tx uft(
-	.clk_i(pclk_i),
+	.clk_i(clk_i),
 	.data_i(tx_data),
-	.rst_ni(prst_ni),
+	.rst_ni(rst_ni),
 	.wr_en(wr_en_tx),
 	.rd_en(rd),
 	.intr_full(intr_tx_full),
@@ -111,9 +111,9 @@ uart_fifo_tx uft(
 
 
 uart_fifo_rx ufr(
-	.clk_i(pclk_i),
+	.clk_i(clk_i),
 	.data_i(rx_data),//rx_data
-	.rst_ni(prst_ni),
+	.rst_ni(rst_ni),
 	.wr_en(rx_fifo_wr),
 	.intr_full(intr_rx_full),
 	.intr_empty(intr_rx_empty),
@@ -123,8 +123,8 @@ uart_fifo_rx ufr(
 );
 
 timer_rx trx(
-	.clk_i(pclk_i),
-	.rst_ni(prst_ni),
+	.clk_i(clk_i),
+	.rst_ni(rst_ni),
 	.baud(baud),
 	.rx_done(rx_done),
 	.c_START(c_START),
@@ -140,8 +140,8 @@ localparam ADDR_TX_EN_FIFO = 12'h014;
 localparam ADDR_TX_FIFO_LEVEL = 12'h018;
 localparam ADDR_RD_EN_TXFIFO = 12'h01c;
 
-always @(posedge pclk_i) begin
-	if (~prst_ni) begin									//When reset is set to 0, it resets all the registers(active low)
+always @(posedge clk_i) begin
+	if (~rst_ni) begin									//When reset is set to 0, it resets all the registers(active low)
 			baud <= 16'd0;
 			rx_en <= 1'b0;
 			rx_data <= 8'd0;
@@ -151,29 +151,29 @@ always @(posedge pclk_i) begin
 			tx_data <= 0;
 			intr_tx <= 0;
 			r_tx_byte_done <= 0;
-			prdata_o <= 0;
+			reg_rdata <= 0;
 	end
 	else begin
-		if(pwrite_i) begin														//When pwrite is set to 1 
-			case(paddr_i)
+		if(reg_we) begin														//When pwrite is set to 1 
+			case(reg_addr)
 				ADDR_BAUD: begin
-					baud[15:0] <= pwdata_i[15:0];						//at address: ADDR_BAUD it will take data from pwdata to confire the baud rate
+					baud[15:0] <= reg_wdata[15:0];						//at address: ADDR_BAUD it will take data from pwdata to confire the baud rate
 				end		
 				
 				ADDR_TX_DATA: begin
-					tx_data <= pwdata_i[7:0];								//at address: ADDR_TX_DATA it will take the data to be transfered
+					tx_data <= reg_wdata[7:0];								//at address: ADDR_TX_DATA it will take the data to be transfered
 				end
 				ADDR_RX_EN:begin
-					rx_en <= pwdata_i[0];										//at address: ADDR_RX_EN it will enable the receiver
+					rx_en <= reg_wdata[0];										//at address: ADDR_RX_EN it will enable the receiver
 				end
 				ADDR_TX_EN_FIFO: begin
-					tx_en_fifo <= pwdata_i[0];							//at address ADDR_TX_EN_FIFO it will enable the tx fifo to write
+					tx_en_fifo <= reg_wdata[0];							//at address ADDR_TX_EN_FIFO it will enable the tx fifo to write
 				end
 				ADDR_TX_FIFO_LEVEL: begin
-					tx_level <= pwdata_i[2:0];							//at address ADDR_TX_FIFO_LEVEL it will set the tx_level
+					tx_level <= reg_wdata[2:0];							//at address ADDR_TX_FIFO_LEVEL it will set the tx_level
 				end
 				ADDR_RD_EN_TXFIFO:begin
-					rd_en_fifo <= pwdata_i[0];							//at address ADDR_RD_EN_TXFIFO it will read the data from tx fifo and enable the transmitter 
+					rd_en_fifo <= reg_wdata[0];							//at address ADDR_RD_EN_TXFIFO it will read the data from tx fifo and enable the transmitter 
 				end
 				default: begin
 						baud <= 16'd0;
@@ -182,16 +182,16 @@ always @(posedge pclk_i) begin
 						tx_level <= 1'b0;
 				end        
 			endcase
-		end	//if(pwrite_i)
-		else if (pread_i) begin												//when pread_i is set to high
-			case(paddr_i)
+		end	//if(reg_we)
+		else if (reg_re) begin												//when reg_re is set to high
+			case(reg_addr)
 				ADDR_RX_DATA: begin
-					prdata_o [7:0] <= rx_fifo_o [7:0];			//at address ADDR_RX_DATA it will read the data from the rx fifo and output to prdata_o
+					reg_rdata [7:0] <= rx_fifo_o [7:0];			//at address ADDR_RX_DATA it will read the data from the rx fifo and output to reg_rdata
 				end
 				default:
-						prdata_o <= 32'd0;
+						reg_rdata <= 32'd0;
 			endcase   
-		end //else if (pread_i)
+		end //else if (reg_re)
 	end
 
 		if (rx_done) begin														//when rx_done triggers 
@@ -223,10 +223,10 @@ always @(posedge pclk_i) begin
 			end
 		end
 
-		pwrite_d <= pwrite_i;
+		pwrite_d <= reg_we;
 end
 
-always @(posedge pclk_i) begin
+always @(posedge clk_i) begin
 	rd_d <= rd;
 	if (rd_d == 1'b1 && rd == 1'b0 ) begin																					
 			tx_en <= 1'b1;																													//tx_en triggers when new byte is to be transfered from the tx_fifo			
@@ -243,7 +243,7 @@ always @(posedge pclk_i) begin
 end
 
 
-always @(posedge pclk_i) begin
+always @(posedge clk_i) begin
 	rx_done_d <= rx_done;
 	if(rx_done == 1'b0 && rx_done_d == 1'b1) begin
 		 rx_fifo_wr <= 1'b1;																										//when each byte is received rx_fifo_wr is set high to write the byte in the rx_fifo
@@ -259,5 +259,5 @@ assign intr_rx_timeout = rx_timeout;
 assign wr_en_tx = tx_en_fifo && pwrite_d;
 assign rd_en_tx = rd_en_fifo && pwrite_d;
 assign intr_rx = (rx_timeout == 1'b1)? 1: 0;
-assign rd_en_rx_fifo=(paddr_i == ADDR_RX_DATA)? 1:0;
+assign rd_en_rx_fifo=(reg_addr == ADDR_RX_DATA)? 1:0;
 endmodule
